b7c758c367d659bee64bf35357ec604f5abf3bc8
Guimts16 <mtsguilherme991@gmail.com>
2023-10-24 17:43:47 -0300

    Terminei

--- api.py
+++ api.py
@@ -1,93 +1,94 @@
-import requests
+import grequests
+import logging
 
-prg = input('Envie o ID ou Nome de um Pokemon: ')
-# URL's
-url = f"https://pokeapi.co/api/v2/pokemon/{prg}/"
-url_legends = f"https://pokeapi.co/api/v2/pokemon-species/{prg}/"
-# JSON's
-poke = requests.get(url)
-legend = requests.get(url_legends)
-# Função
-def Pokemon(start: int, end: int):
+logging.basicConfig(level=logging.INFO, format='- %(name)s - %(levelname)s - %(message)s')
+
+def pokemon(pokemon_id):
+    logger = logging.getLogger(f'Pokemon #{pokemon_id}')
+
+    url = f"https://pokeapi.co/api/v2/pokemon/{pokemon_id}/"
+    url_legends = f"https://pokeapi.co/api/v2/pokemon-species/{pokemon_id}/"
+
+    responses = grequests.map([grequests.get(url), grequests.get(url_legends)])
+
+    poke = responses[0]
+    legend = responses[1]
+
     if poke.status_code == 200:
         data = poke.json()
-        print(f"ID: {data['id']}")
-        print(f"Nome: {data['name']}")
-        print(f"Altura: {data['height']}")
-        print(f"Peso: {data['weight']}")
-        print("Tipo: ", end='')
+        logger.info(f"ID: {data['id']}")
+        logger.info(f"Nome: {data['name']}")
+        logger.info(f"Altura: {data['height']}")
+        logger.info(f"Peso: {data['weight']}")
+        tipo_string = "Tipo: "
         for tipo in data["types"]:
-            print(f"{tipo['type']['name']}/", end='')
-        print()
-# Ataques
-    if poke.status_code == 200:
-        dete = poke.json()
-        decrease_moves = dete["moves"]
+            tipo_string += f"{tipo['type']['name']}/"
+        logger.info(tipo_string)
 
-# Not Power None
-    if decrease_moves is not None:
-        print("Ataques: ", end='')
+        decrease_moves = data["moves"]
 
-        max_moves = 3
-        moves_printed = 0
-        for movim in decrease_moves:
-            if moves_printed >= max_moves:
-                break
+        if decrease_moves is not None:
+            ataques_string = "Ataques: "
+            max_moves = 3
+            moves_printed = 0
+            for movim in decrease_moves:
+                if moves_printed >= max_moves:
+                    break
 
-            url_moves = movim["move"]["url"]
-            moves = requests.get(url_moves)
-            poder = moves.json()
-            poderes = poder["power"]
-            if poderes is not None:
-                move_name = movim["move"]["name"]
-                print(f"{move_name} / ", end='')
-                moves_printed += 1
-        print()
-# Poderes
-    if decrease_moves is not None:
-        print("Poderes: ", end='')
+                url_moves = movim["move"]["url"]
+                moves = grequests.map([grequests.get(url_moves)])[0]
+                poder = moves.json()
+                poderes = poder["power"]
+                if poderes is not None:
+                    move_name = movim["move"]["name"]
+                    ataques_string += f"{move_name} / "
+                    moves_printed += 1
+            logger.info(ataques_string)
 
-        max_moves = 3
-        moves_printed = 0
-        for movim in decrease_moves:
-            if moves_printed >= max_moves:
-                break
+        if decrease_moves is not None:
+            poderes_string = "Poderes: "
+            max_moves = 3
+            moves_printed = 0
+            for movim in decrease_moves:
+                if moves_printed >= max_moves:
+                    break
 
-            url_moves = movim["move"]["url"]
-            moves = requests.get(url_moves)
-            poder = moves.json()
-            poderes = poder["power"]
-            if poderes is not None:
+                url_moves = movim["move"]["url"]
+                moves = grequests.map([grequests.get(url_moves)])[0]
+                poder = moves.json()
+                poderes = poder["power"]
+                if poderes is not None:
+                    poderes_string += f"{poderes}/"
+                    moves_printed += 1
+            logger.info(poderes_string)
 
-                print(f"{poderes}/", end='')
-                moves_printed += 1
-    print()
-# Status
-    vida = data["stats"]
+        vida = data["stats"]
 
-    if vida is not None:
-        for life in vida:
-            live = life["stat"]["name"]
-            amor = life["base_stat"] 
-            if live == "hp":
-                print(f"{live}: {amor}")
+        if vida is not None:
+            for life in vida:
+                live = life["stat"]["name"]
+                amor = life["base_stat"]
+                if live == "hp":
+                    logger.info(f"{live}: {amor}")
 
-    if vida is not None:
-        for life in vida:
-            live = life["stat"]["name"]
-            amor = life["base_stat"] 
-            if live == "attack":
-                print(f"{live}: {amor}")
+        if vida is not None:
+            for life in vida:
+                live = life["stat"]["name"]
+                amor = life["base_stat"]
+                if live == "attack":
+                    logger.info(f"{live}: {amor}")
 
-    if vida is not None:
-        for life in vida:
-            live = life["stat"]["name"]
-            amor = life["base_stat"] 
-            if live == "defense":
-                print(f"{live}: {amor}")
-# Lendario?
+        if vida is not None:
+            for life in vida:
+                live = life["stat"]["name"]
+                amor = life["base_stat"]
+                if live == "defense":
+                    logger.info(f"{live}: {amor}")
+
     if legend.status_code == 200:
         lege = legend.json()
-        print(f"Lendario: {lege['is_legendary']}")
-# Incia a função
-Pokemon(1, 151)
+        logger.info(f"Lendario: {lege['is_legendary']}\n========")
+
+# Loop 
+for i in range(1, 152):
+    pokemon(i)
